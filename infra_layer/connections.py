6acc6ee361af6f1e44062d6ca3e326f71682ffdc
Denys Bedilov <denbedilov@gmail.com>
2022-01-31 11:22:15 +0200

    added class for future import and try+catch for connection error

--- infra_layer/connections.py
+++ infra_layer/connections.py
@@ -1,19 +1,35 @@
 import requests
 
-url = "http://127.0.0.1:3002/"
 
-api = {
-    'users': {
-        'add_friend': 'users/add_friend',
-        'add_playlist': 'users/add_playlist',
-        'add_user': 'users/add_user',
-        'change_password': 'users/change_password',
-        'get_playlist': 'users/get_playlist',
-        'get_user': 'users/get_user'
+class APIConnections:
+    url = "http://127.0.0.1:3002/"
+    api = {
+        'users': {
+            'add_friend': 'users/add_friend',
+            'add_playlist': 'users/add_playlist',
+            'add_user': 'users/add_user',
+            'change_password': 'users/change_password',
+            'get_playlist': 'users/get_playlist',
+            'get_user': 'users/get_user'
+        }
     }
-}
+    params = {
+        'get_user': '?user_name='
+    }
+    timeout = 0.01
 
-params = '?user_name='
-username = 'Arnold'
-response = requests.get(url + api['users']['get_user'] + params + username)
-print(response.json())
+    def get_user(self, username):
+        try:
+            req = requests.get(con.url + con.api['users']['get_user'] + con.params['get_user'] + username,
+                               timeout=self.timeout)
+            if req.status_code == 200:
+                return req.json()
+            else:
+                return 'Something was wrong with getting user'
+        except requests.ConnectionError:
+            return 'There is no connection to the server'
+
+
+con = APIConnections()
+user = 'Arnold'
+print(con.get_user(user))
