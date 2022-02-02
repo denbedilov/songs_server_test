174663a3ebca9b6fecf3e546dabaf6bb1b2e3c10
Denys Bedilov <DENYSBE@rafael.co.il>
2022-02-02 08:25:34 +0200

    implements test_add_playlist implements test_get_playlist added next marks: users, songs, playlists for their test actions

--- logic_layer/data.py
+++ logic_layer/data.py
@@ -27,3 +27,14 @@ user_with_pass = {
 
 # user to get after password change
 get_user_with_pass = {'data': {'friends': [], 'playlists': [], 'user_name': 'user_with_pass'}, 'message': 'OK'}
+
+# user for adding playlist
+user_with_playlist = {
+    'user_name': 'user_with_playlist',
+    'user_password': 'pass',
+    'playlist_name': 'new_playlist'
+}
+
+# user to get after adding playlist
+get_user_with_playlist = {'data': {'friends': [], 'playlists': ['new_playlist'], 'user_name': 'user_with_playlist'},
+                          'message': 'OK'}
--- test_layer/test_user.py
+++ test_layer/test_user.py
@@ -1,11 +1,11 @@
 import pytest
-
 from logic_layer import users, data
 
 
 # test basic user actions
 # test add new user
 @pytest.mark.basic
+@pytest.mark.users
 def test_add_user(remove_users):
     users.add_user(data.user)
     res = users.get_user(data.user)
@@ -15,6 +15,7 @@ def test_add_user(remove_users):
 
 # test get user
 @pytest.mark.basic
+@pytest.mark.users
 def test_get_user(remove_users):
     users.add_user(data.user)
     res = users.get_user(data.user)
@@ -24,6 +25,7 @@ def test_get_user(remove_users):
 
 # test add friend
 @pytest.mark.basic
+@pytest.mark.users
 def test_add_friend(remove_users):
     users.add_user(data.user_with_friend)
     users.add_friend(data.user_with_friend)
@@ -34,8 +36,30 @@ def test_add_friend(remove_users):
 
 # test change password
 @pytest.mark.basic
+@pytest.mark.users
 def test_change_password(remove_users):
     users.add_user(data.user_with_pass)
     res = users.change_password(data.user_with_pass)
     expected = data.get_user_with_pass
+    assert res, expected
+    # TODO add another check? something needs a new password
+
+
+# test add playlist
+@pytest.mark.basic
+@pytest.mark.users
+def test_add_playlist(remove_users):
+    users.add_user(data.user_with_playlist)
+    res = users.get_user(data.user_with_playlist)
+    expected = data.get_user_with_playlist
+    assert res, expected
+
+
+# test get playlist
+@pytest.mark.basic
+@pytest.mark.users
+def test_get_playlist(remove_users):
+    users.add_user(data.user_with_playlist)
+    res = users.get_user(data.user_with_playlist)
+    expected = data.get_user_with_playlist
     assert res, expected
